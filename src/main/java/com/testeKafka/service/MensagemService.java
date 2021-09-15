4afc0b38bf90ce5eb65bcae8f2c916175ae33645
JJeferson <48735241+JJeferson@users.noreply.github.com>
2021-09-15 15:00:30 -0300

    Envio Funcionou

--- src/main/java/com/testeKafka/service/MensagemService.java
+++ src/main/java/com/testeKafka/service/MensagemService.java
@@ -30,7 +30,7 @@ public class MensagemService implements MensagemInterface{
     }
     private void EnviaMSG(Mensagem mensagem) throws ExecutionException, InterruptedException {
 
-        var producer = new KafkaProducer<String,String>(properties());
+        var producer = new KafkaProducer<String,String>(propertiesEnvio());
         var record = new ProducerRecord<>("Topico_De_Teste",
                 mensagem.getIdMensagem(),mensagem.getTextoMensagem());
 
@@ -48,7 +48,7 @@ public class MensagemService implements MensagemInterface{
 
     }
 
-    private static Properties properties(){
+    private static Properties propertiesEnvio(){
         var properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
